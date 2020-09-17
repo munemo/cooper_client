10dcf840b3aed991249661b6f53876a6146a5586
SebastianN97 <sebbe132132@gmail.com>
2020-09-17 12:09:45 +0200

    Creating component LoginForm.jsx. Modifying App.jsx after new component. Creating user can log in spec.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
 import React, { Component } from "react";
-
 import DisplayCooperResult from "./components/DisplayCooperResult";
 import InputFields from "./components/InputFields";
+import LoginForm from "./components/LoginForm";
 
 class App extends Component {
   state = {
     distance: "",
     gender: "female",
     age: "",
+    renderLoginForm: false
   };
 
-  onChangeHandler = (e) => {
+  onChangeHandler = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
   render() {
+    const renderLogin = this.state.renderLoginForm ? (
+      <LoginForm />
+    ) : (
+      <button
+        id="login"
+        onClick={() => this.setState({ renderLoginForm: true })}
+      >
+        Login
+      </button>
+    );
     return (
       <>
         <InputFields onChangeHandler={this.onChangeHandler} />
+        {renderLogin}
         <DisplayCooperResult
           distance={this.state.distance}
           gender={this.state.gender}
